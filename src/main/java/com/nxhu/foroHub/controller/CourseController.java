80c3f5cdbd13e0aa168257c5325837e81032ed54
nxhuel <nahueltisera03@gmail.com>
2024-12-16 23:55:52 -0300

    foroHub: Controllador de course finalizado

---
+++ src/main/java/com/nxhu/foroHub/controller/CourseController.java
@@ -0,0 +1,68 @@
+package com.nxhu.foroHub.controller;
+
+import com.nxhu.foroHub.dto.CourseDTO;
+import com.nxhu.foroHub.persistence.entity.CourseEntity;
+import com.nxhu.foroHub.service.CourseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/v1")
+public class CourseController
+{
+    @Autowired
+    private CourseService courseService;
+
+    @PostMapping("/create-course")
+    public ResponseEntity<Void> createCourse(@RequestBody CourseEntity aCourse)
+    {
+        courseService.createCourse(aCourse);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @GetMapping("/courses")
+    public ResponseEntity<Set<CourseDTO>> getCourses()
+    {
+        boolean notFound = courseService.getCourse().isEmpty();
+        if (notFound) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        Set<CourseDTO> courses = courseService.getCourse().stream()
+                .map(c -> new CourseDTO(c.getName(), c.getCategory()))
+                .collect(Collectors.toSet());
+
+        return new ResponseEntity<>(courses, HttpStatus.OK);
+    }
+
+    @GetMapping("/course/{id}")
+    public ResponseEntity<CourseDTO> getCourse(@PathVariable Long courseId)
+    {
+        boolean notFound = courseService.getCourseById(courseId).isEmpty();
+        if (notFound) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        return courseService.getCourseById(courseId)
+                .map(c -> new CourseDTO(c.getName(), c.getCategory()))
+                .map(courseDTO -> new ResponseEntity<>(courseDTO, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @DeleteMapping("/delete-course/{id}")
+    public ResponseEntity<Void> deleteCourse(@PathVariable Long courseId)
+    {
+        boolean notFound = courseService.getCourseById(courseId).isEmpty();
+        if (notFound) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        courseService.deleteCourseById(courseId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
