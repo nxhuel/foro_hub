7cfe056331293d6e44e7d720c023168c49e4fc9b
nxhuel <nahueltisera03@gmail.com>
2024-12-16 23:43:06 -0300

    foroHub: Controllador de temas finalizado

--- src/main/java/com/nxhu/foroHub/controller/TopicController.java
+++ src/main/java/com/nxhu/foroHub/controller/TopicController.java
@@ -12,7 +12,7 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/v1")
 public class TopicController
 {
     @Autowired
@@ -28,10 +28,53 @@ public class TopicController
     @GetMapping("/topics")
     public ResponseEntity<Set<TopicDTO>> getTopics()
     {
+        boolean notFound = topicService.getTopics().isEmpty();
+        if (notFound) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
         Set<TopicDTO> topics = topicService.getTopics().stream()
                 .map(t -> new TopicDTO(t.getTitle(), t.getMessage(), t.getCreation_date(), t.getStatus(), t.getAuthor(), t.getCourse()))
                 .collect(Collectors.toSet());
 
         return new ResponseEntity<>(topics, HttpStatus.OK);
+    }
+
+    @GetMapping("/topic/{id}")
+    public ResponseEntity<TopicDTO> getTopic(@PathVariable Long topicId)
+    {
+        boolean notFound = topicService.getTopicById(topicId).isEmpty();
+        if (notFound) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        return topicService.getTopicById(topicId)
+                .map(t -> new TopicDTO(t.getTitle(), t.getMessage(), t.getCreation_date(), t.getStatus(), t.getAuthor(), t.getCourse()))
+                .map(topicDTO -> new ResponseEntity<>(topicDTO, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PutMapping("/update-topic/{id}")
+    public ResponseEntity<Void> updateTopic(@PathVariable Long topicId, @RequestBody TopicEntity newTopic)
+    {
+        boolean notFound = topicService.getTopicById(topicId).isEmpty();
+        if (notFound) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        topicService.updateTopic(topicId, newTopic);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping("/delete-topic/{id}")
+    public ResponseEntity<Void> deleteTopic(@PathVariable Long topicId)
+    {
+        boolean notFound = topicService.getTopicById(topicId).isEmpty();
+        if (notFound) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        topicService.deleteTopicById(topicId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
